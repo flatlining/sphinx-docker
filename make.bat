f953515404cb29c073dd9d5f9c59c76779fcad7f
Matias S <mschertel@gmail.com>
2018-08-25 22:19:58 -0300

    improve windows make

--- make.bat
+++ make.bat
@@ -7,6 +7,10 @@ if "%DOCKERCMD%" == "" (
 )
 
 if "%1" == "" goto help
+if "%1" == "build" goto build
+if "%1" == "remove" goto remove
+if "%1" == "quickstart" goto quickstart
+if "%1" == "bash" goto bash
 
 %DOCKERCMD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -22,6 +26,22 @@ goto end
 
 :help
 %DOCKERCMD% run --rm -it -v %~dp0/doc:/doc sphinx:latest help
+goto end
+
+:build
+%DOCKERCMD% build -t sphinx:latest .
+goto end
+
+:remove
+%DOCKERCMD% container rm sphinx
+goto end
+
+:quickstart
+%DOCKERCMD% run --rm -it --name sphinx -v %~dp0/doc:/doc --entrypoint "sphinx-quickstart" sphinx:latest
+goto end
+
+:bash
+%DOCKERCMD% run --rm -it --name sphinx -v %~dp0/doc:/doc --entrypoint "/bin/bash" sphinx:latest
 goto end
 
 :end
