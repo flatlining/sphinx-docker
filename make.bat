b97fb99f0d0fb4eb8909bfffeff1c31d670a0626
Matias S <mschertel@gmail.com>
2018-08-26 19:36:08 -0300

    improve windows make

--- make.bat
+++ make.bat
@@ -6,20 +6,19 @@ if "%DOCKERCMD%" == "" (
 	set DOCKERCMD=docker
 )
 
+%DOCKERCMD% >NUL 2>NUL
+if errorlevel 9009 (
+	echo.
+	echo.The '%DOCKERCMD%' command was not found. Make sure you have Docker
+	echo.installed and the daemon is running.
+	exit /b 1
+)
+
 if "%1" == "" goto help
 if "%1" == "build" goto build
 if "%1" == "remove" goto remove
 if "%1" == "quickstart" goto quickstart
 if "%1" == "bash" goto bash
-
-%DOCKERCMD% >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo.The 'docker' command was not found. Make sure you have Docker
-	echo.installed and the daemon is running.
-	echo.
-	exit /b 1
-)
 
 %DOCKERCMD% run --rm -it --name sphinx -v %~dp0/doc:/doc sphinx:latest %1
 goto end
@@ -37,6 +36,7 @@ goto end
 goto end
 
 :quickstart
+mkdir doc
 %DOCKERCMD% run --rm -it --name sphinx -v %~dp0/doc:/doc --entrypoint "sphinx-quickstart" sphinx:latest
 goto end
 
